c256976a98e1be250006436f8046105379329648
Faisal Imtiyaz <faisalimtiyaz.fi@gmail.com>
2022-12-01 20:49:29 +0530

    testing github cli

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+document.getElementById("btn").onclick =function(){
+     var textEntered="";
+
+    textEntered=document.getElementById("text").value;
+    
+    document.getElementById("head").innerHTML=textEntered;
+
+}
+alert("Hello faisal");
